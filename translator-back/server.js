e2635c7968ebe04e4001497013d19df74514d2c3
LordRaina <trinhjacky@yahoo.fr>
2020-06-30 17:10:36 +0200

    Added cors library for back

--- translator-back/server.js
+++ translator-back/server.js
@@ -1,17 +1,23 @@
-var express = require('express'),
+const express = require('express'),
   app = express(),
   port = process.env.PORT || 3000;
   bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+const cors = require('cors');
 
-var routes = require('./api/routes/translatorRoutes');
+// use it before all route definitions
+app.use(cors({origin: 'http://localhost:8080'}));
+const routes = require('./api/routes/translatorRoutes');
 routes(app);
 
 app.listen(port);
-app.use(function(req, res) {
+
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.status(404).send({url: req.originalUrl + ' not found'})
 });
+
 
 console.log('RESTful API server started on: ' + port);
